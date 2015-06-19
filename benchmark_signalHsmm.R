ef37a8f5ba77ff6a8424cd39fabaa01034cf5770
michbur <michalburdukiewicz@gmail.com>
2015-06-19 13:29:01 +0200

    ready for dr Mackiewicz

--- benchmark_signalHsmm.R
+++ benchmark_signalHsmm.R
@@ -80,7 +80,7 @@ parametersSet5 <- add_k_mer_state2(kMer4, pipar = parametersSet4$pipar, tpmpar =
 overall_probs_log <- signalHsmm2010$overall_probs_log
 maxSignal <- 45
 
-registerDoMC(cores=6)
+registerDoMC(cores=4)
 n <- length(benchmark_data2)
 results <- foreach(i=1:n) %dopar% {
   prot <- benchmark_data2[[i]]
@@ -158,9 +158,42 @@ all_preds_plas <- data.frame(other_soft_plas,
                              signalHsmm2010 = pred2df(predict(signalHsmm2010, benchmark_plas_data))[["sp.probability"]],
                              signalHsmm1989 = pred2df(predict(signalHsmm1989, benchmark_plas_data))[["sp.probability"]])
 
+registerDoMC(cores=4)
+n <- length(benchmark_plas_data)
+results <- foreach(i=1:n) %dopar% {
+  prot <- benchmark_plas_data[[i]]
+  deg_sample <- na.omit(as.numeric(degenerate(toupper(prot)[1L:min(maxSignal, length(prot))], group_best)))
+  
+  viterbi_res <- duration_viterbi2(deg_sample-1, signalHsmm2010$pipar, signalHsmm2010$tpmpar, 
+                                   signalHsmm2010$od, signalHsmm2010$params)
+  viterbi_path <- viterbi_res[["path"]]+1
+  c_site <- ifelse(any(viterbi_path == 4), max(which(viterbi_path == 3)), length(deg_sample))
+  prob.signal <- viterbi_res[["viterbi"]][c_site, viterbi_path[c_site]]
+  prob.non <- Reduce(function(x, y) x + overall_probs_log[y], deg_sample[1L:c_site], 0)
+  prob.total <- exp(prob.signal - prob.non)
+  
+  viterbi_res5 <- duration_viterbi2(deg_sample-1, parametersSet5$pipar, parametersSet5$tpmpar, 
+                                    parametersSet5$od, parametersSet5$params)
+  viterbi_path5 <- viterbi_res5[["path"]]+1
+  c_site5 <- ifelse(any(viterbi_path5 == 4), min(which(viterbi_path5 == 4))-1, length(deg_sample))
+  c_site5 <- ifelse(any(viterbi_path5 == lastState1), min(which(viterbi_path5 == lastState1)), c_site5)
+  c_site5 <- ifelse(any(viterbi_path5 == lastState2), min(which(viterbi_path5 == lastState2)), c_site5)
+  c_site5 <- ifelse(any(viterbi_path5 == lastState3), min(which(viterbi_path5 == lastState3)), c_site5)
+  c_site5 <- ifelse(any(viterbi_path5 == lastState4), min(which(viterbi_path5 == lastState4)), c_site5)
+  prob.signal5 <- viterbi_res5[["viterbi"]][c_site5, viterbi_path5[c_site5]]
+  prob.non5 <- Reduce(function(x, y) x + overall_probs_log[y], deg_sample[1L:c_site5], 0)
+  prob.total5 <- exp(prob.signal5 - prob.non5)
+  
+  c(1 - 1/(1 + prob.total), 1 - 1/(1 + prob.total5), c_site, c_site5)
+}
+probs <- t(sapply(results, function (x) head(x, 2)))
+cut <- t(sapply(results, function (x) tail(x, 2)))
+
+all_preds_plas$signalKmer = probs[,2]
+
 
 bench_metrics <- calc_mcc(HMeasure(real_labels_plas, all_preds_plas,
-                                   threshold = c(rep(0.5, 4), 0.05, 0.05))[["metrics"]])
+                                   threshold = c(rep(0.5, 4), 0.05, 0.05, 0.05))[["metrics"]])
 
 bench_metrics <- data.frame(rownames(bench_metrics), bench_metrics[, c("AUC", "Sens", "Spec", "mcc")])
 colnames(bench_metrics) <- c("Software name", "AUC", "Sensitivity", "Specificity", "MCC")
@@ -168,7 +201,8 @@ colnames(bench_metrics) <- c("Software name", "AUC", "Sensitivity", "Specificity
 levels(bench_metrics[["Software name"]]) <- c("Philius \\citep{2008reynoldstransmembrane}", "Phobius \\citep{2004klla}", 
                                               "PrediSi \\citep{2004hillerpredisi}", 
                                               "signalHsmm-1989", 
-                                              "signalHsmm-2010", 
+                                              "signalHsmm-2010",
+                                              "signalHsmm-2010 with k-mers",
                                               "signalP 4.1 (no tm) \\citep{2011petersensignalp}", 
                                               "signalP 4.1 (tm) \\citep{2011petersensignalp}")
 
